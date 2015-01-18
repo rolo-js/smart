e75a736ad01b0a4f3610113c5b9ee8fa29cc01ac
Rolando Rios <rriosarias@gmail.com>
2015-01-18 17:03:55 -0600

    Smart Admin Tepmplate added. Some unsused files from express generator removed

--- app/routes.js
+++ app/routes.js
@@ -68,7 +68,6 @@ module.exports = function(app, passport) {
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-  console.log(req);
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
     return next();
--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 
@@ -43,6 +42,7 @@ app.set('view engine', 'hbs');
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // catch 404 and forward to error handler
